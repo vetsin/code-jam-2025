27c46894312d4da6f0e58992b3b996fcccbb82e8
Ethanscharlie <ethanscharlie@protonmail.com>
2025-08-08 14:05:35 -0600

    Some basic ui code is working

--- src/password_manager/app/__init__.py
+++ src/password_manager/app/__init__.py
@@ -4,6 +4,8 @@ This module figures out how to tie everything together and export the app.
 """
 
 from nicegui import ui
+from ..components.passcode_factories.map import map_lock_factory
+from ..components.passcode_factories import Passcode
 
 
 def app() -> ui.element:
@@ -26,6 +28,20 @@ def app() -> ui.element:
             # Card content
             ui.label("content")
 
+    def launch_map_lock_passcode() -> None:
+        """Launch the map lock passcode input inside login_container."""
+        with login_container:
+            ui.separator()
+            ui.label("Map Lock")
+            map_lock_factory(submit_passcode=handle_passcode_submission)
+
+    def handle_passcode_submission(code: Passcode) -> None:
+        """Handle what happens when a passcode is submitted."""
+        if code == 1234:
+            ui.notify("Access granted", color="positive")
+        else:
+            ui.notify("Wrong code", color="negative")
+
     def item_to_spawn_new_passcode() -> None:
         """Just a placeholder.
 
@@ -41,6 +57,7 @@ def app() -> ui.element:
             # for each passcode_factory in password_manager.components.passcode_factories,
             #     item_to_spawn_new_passcode(passcode_factory) ig
             item_to_spawn_new_passcode()
+            ui.item("Map Lock", on_click=launch_map_lock_passcode)
             ui.item("add a thing", on_click=add)
             ui.item("Item 2", on_click=lambda: ui.notify("You clicked item 2"))
 
--- src/password_manager/components/passcode_factories/map.py
+++ src/password_manager/components/passcode_factories/map.py
@@ -1,7 +1,19 @@
 from collections.abc import Callable
+from nicegui import ui
 
 from . import Passcode
 
 
 def map_lock_factory(submit_passcode: Callable[[Passcode], None]) -> None:
     """See https://github.com/vetsin/code-jam-2025/issues/6."""
+
+    passcode_input = ui.number(label='Enter Passcode', format='0', step=1)
+
+    def handle_submit():
+        try:
+            value = int(passcode_input.value)
+            submit_passcode(value)
+        except (ValueError, TypeError):
+            ui.notify('Please enter a valid number', color='negative')
+
+    ui.button('Unlock', on_click=handle_submit)
