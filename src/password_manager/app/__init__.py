4b4068928e3ffee06e86bce6402ab5d74dd7c751
Wade Cheng <86323079+wade-cheng@users.noreply.github.com>
2025-08-08 18:52:23 -0400

    feat: add the login component (#15)
    also put that component on the main page, which prints out whatever passcodes are submitted.

--- src/password_manager/app/__init__.py
+++ src/password_manager/app/__init__.py
@@ -5,43 +5,24 @@ This module figures out how to tie everything together and export the app.
 
 from nicegui import ui
 
+from password_manager.components import login
+from password_manager.components.passcode_factories import Passcode
+
+
+def temp_submit_passcode_check(p: Passcode) -> None:
+    """Just print out the passcode.
+
+    In the full app, this might hash the passcode, validate against the user,
+    and go to the next screen if it validates.
+    """
+    print(f"we received the passcode {p}")
+
 
 def app() -> ui.element:
     """The main ui element representing the entire app."""
     login_container = ui.column().classes("self-center")
 
-    def add() -> None:
-        """Add a removable card to the login container.
-
-        This is just me testing out cool test code. Neat that this works.
-        """
-        with login_container, ui.card() as card:
-            # Header with title and close button
-            with ui.row().classes("w-full justify-between items-center mb-2"):
-                ui.label("title").classes("text-lg font-semibold")
-                ui.button(icon="close", on_click=lambda: card.delete()).props("flat round size=sm").classes(
-                    "text-gray-500 hover:text-gray-700",
-                )
-
-            # Card content
-            ui.label("content")
-
-    def item_to_spawn_new_passcode() -> None:
-        """Just a placeholder.
-
-        This should probably be in `password_manager` and take a password_factories.PasscodeFactory
-        and return a ui.item that on click, spawns the passcode.
-        """
-        ui.item("temp", on_click=lambda: ui.notify("You clicked the temp thing."))
-
     with login_container as app:
-        ui.label("What kind of passcode?")
-
-        with ui.dropdown_button("Open me!", auto_close=True):
-            # for each passcode_factory in password_manager.components.passcode_factories,
-            #     item_to_spawn_new_passcode(passcode_factory) ig
-            item_to_spawn_new_passcode()
-            ui.item("add a thing", on_click=add)
-            ui.item("Item 2", on_click=lambda: ui.notify("You clicked item 2"))
+        login.login(temp_submit_passcode_check)
 
     return app
---
+++ src/password_manager/components/login/__init__.py
@@ -0,0 +1,44 @@
+from collections.abc import Callable
+
+from nicegui import ui
+
+from password_manager.components.passcode_factories import Passcode, PasscodeInputFactory
+from password_manager.components.passcode_factories.binary import binaryinput_factory
+from password_manager.components.passcode_factories.text import textinput_factory
+
+
+def login(submit_passcode: Callable[[Passcode], None]) -> ui.element:
+    """Component for logging in.
+
+    - We have a dropdown to select passcode inputs from `passcode_factories`.
+    - When we select an input type from the dropdown, we spawn the corresponding passcode input.
+    - Multiple inputs can be active at a time.
+        - If we don't like this, we might refactor so that only one input is active at a time.
+          If another is selected, we despawn the old input before spawning the new input.
+    - Like the individual inputs, we defer the decision on whether an input succeeded or failed
+      to our caller. To do so, we forward our `submit_passcode` function into the spawned input.
+    """
+    with ui.column() as login, ui.dropdown_button("Open me!", auto_close=True) as dropdown:
+
+        def _add(elem: Callable[[], ui.element]) -> None:
+            """Spawn an element into login wrapped in a closable card."""
+            with login, ui.card() as card, ui.row().classes("w-full justify-between items-center mb-2"):
+                elem()
+                ui.button(icon="close", on_click=lambda: card.delete()).props("flat round size=sm").classes(
+                    "text-gray-500 hover:text-gray-700",
+                )
+
+        def _spawn_new_passcode_as_item(
+            name: str,
+            spawn_pcode_input: PasscodeInputFactory,
+            submit_passcode: Callable[[Passcode], None],
+        ) -> None:
+            """Spawn a passcode as a named item into the dropdown. It should create a new passcode on select."""
+            with dropdown:
+                ui.item(name, on_click=lambda: _add(elem=lambda: spawn_pcode_input(submit_passcode)))
+
+        _spawn_new_passcode_as_item("Text Input", textinput_factory, submit_passcode)
+        _spawn_new_passcode_as_item("Binary Input", binaryinput_factory, submit_passcode)
+        # spawn more input types as we code them
+
+    return login
