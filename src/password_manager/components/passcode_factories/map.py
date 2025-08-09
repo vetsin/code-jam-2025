207d103794d5ddd4870ce7cafd8372f4bbb0da67
Ethanscharlie <ethanscharlie@protonmail.com>
2025-08-09 12:38:54 -0600

    Satified ruff

--- src/password_manager/components/passcode_factories/map.py
+++ src/password_manager/components/passcode_factories/map.py
@@ -1,45 +1,50 @@
 from collections.abc import Callable
-from nicegui import events, ui
 from struct import pack
 
+from nicegui import events, ui
+
 from . import Passcode
+
+MARKER_AT_ZERO_ERROR = "Marker cannot be at 0x0"
 
 
 class MapLock:
     """See https://github.com/vetsin/code-jam-2025/issues/6."""
 
-    def __init__(self, submit_passcode: Callable[[Passcode], None]):
+    def __init__(self, submit_passcode: Callable[[Passcode], None]) -> None:
         self.submit_passcode = submit_passcode
 
         self.map = ui.leaflet(center=(51.505, -0.090), zoom=3)
-        self.map.on("map-click", self.on_map_input)
+        self.map.on("map-click", self.__on_map_input)
 
         self.marker = self.map.marker(latlng=(0, 0))
         self.latitude = 0
         self.longitude = 0
 
-        self.unlock_button = ui.button("Unlock", on_click=lambda: self.handle_unlock())
+        self.unlock_button = ui.button("Unlock", on_click=lambda: self.__handle_unlock())
 
-    def handle_unlock(self):
+    def __handle_unlock(self) -> None:
         try:
-            value = self.get_passcode_from_placement()
+            value = self.__get_passcode_from_placement()
             self.submit_passcode(value)
         except (ValueError, TypeError):
             ui.notify("Please enter a valid place", color="negative")
 
-    def on_map_input(self, e: events.GenericEventArguments) -> None:
+    def __on_map_input(self, e: events.GenericEventArguments) -> None:
         self.latitude = e.args["latlng"]["lat"]
         self.longitude = e.args["latlng"]["lng"]
-        self.set_marker_to_location()
+        self.__set_marker_to_location()
 
-    def set_marker_to_location(self):
+    def __set_marker_to_location(self) -> None:
         self.marker.move(self.latitude, self.longitude)
 
-    def get_passcode_from_placement(self) -> Passcode:
+    def __get_passcode_from_placement(self) -> Passcode:
         if self.latitude == 0 and self.longitude == 0:
-            raise ValueError("Cannot be Zero")
+            raise ValueError(MARKER_AT_ZERO_ERROR)
         return pack(">ff", self.latitude, self.longitude)
 
+
 def map_input_factory(submit_passcode: Callable[[Passcode], None]) -> ui.element:
+    """Factory for map input."""
     map_lock = MapLock(submit_passcode)
     return map_lock.unlock_button
