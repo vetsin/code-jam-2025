fff5b58b50b217ee751449fcc67c7a722d25f9c0
Ethanscharlie <ethanscharlie@protonmail.com>
2025-08-09 12:01:52 -0600

    Moved map input into new system

--- src/password_manager/components/login/__init__.py
+++ src/password_manager/components/login/__init__.py
@@ -6,6 +6,7 @@ from password_manager.components.passcode_factories import Passcode, PasscodeInp
 from password_manager.components.passcode_factories.binary import binaryinput_factory
 from password_manager.components.passcode_factories.text import textinput_factory
 from password_manager.components.passcode_factories.typst import typstinput_factory
+from password_manager.components.passcode_factories.map import MapLock
 
 
 def login(submit_passcode: Callable[[Passcode], None]) -> ui.element:
@@ -41,6 +42,7 @@ def login(submit_passcode: Callable[[Passcode], None]) -> ui.element:
         _spawn_new_passcode_as_item("Text Input", textinput_factory, submit_passcode)
         _spawn_new_passcode_as_item("Binary Input", binaryinput_factory, submit_passcode)
         _spawn_new_passcode_as_item("Typst Input", typstinput_factory, submit_passcode)
+        _spawn_new_passcode_as_item("Map Input", MapLock, submit_passcode)
         # spawn more input types as we code them
 
     return login
--- src/password_manager/components/passcode_factories/map.py
+++ src/password_manager/components/passcode_factories/map.py
@@ -4,6 +4,7 @@ import struct
 
 from . import Passcode
 
+
 class MapLock:
     """See https://github.com/vetsin/code-jam-2025/issues/6."""
 
@@ -11,13 +12,13 @@ class MapLock:
         self.submit_passcode = submit_passcode
 
         self.map = ui.leaflet(center=(51.505, -0.090), zoom=3)
-        self.map.on('map-click', self.on_map_input)
+        self.map.on("map-click", self.on_map_input)
 
         self.marker = self.map.marker(latlng=(0, 0))
         self.latitude = 0
         self.longitude = 0
 
-        ui.button('Unlock', on_click=lambda: self.handle_unlock())
+        ui.button("Unlock", on_click=lambda: self.handle_unlock())
 
     def handle_unlock(self):
         try:
@@ -25,16 +26,17 @@ class MapLock:
             self.submit_passcode(value)
         except (ValueError, TypeError):
             pass
-            ui.notify('Please enter a valid place', color='negative')
-            
+            ui.notify("Please enter a valid place", color="negative")
+
     def on_map_input(self, e: events.GenericEventArguments) -> None:
-        self.latitude = e.args['latlng']['lat']
-        self.longitude = e.args['latlng']['lng']
+        self.latitude = e.args["latlng"]["lat"]
+        self.longitude = e.args["latlng"]["lng"]
         self.set_marker_to_location()
 
     def set_marker_to_location(self):
         self.marker.move(self.latitude, self.longitude)
 
     def get_passcode_from_placement(self) -> Passcode:
-        if self.latitude == 0 and self.longitude == 0: raise ValueError("Cannot be Zero")
-        return struct.pack('>ff', self.latitude, self.longitude)
+        if self.latitude == 0 and self.longitude == 0:
+            raise ValueError("Cannot be Zero")
+        return struct.pack(">ff", self.latitude, self.longitude)
