2ca3afc4a3fd33895b4ea0334e1fa4e6128ca12f
wade-cheng <wadecheng3301@gmail.com>
2025-08-17 18:40:39 -0700

    vaultKeyValues should have hidden and only string value

--- src/password_manager/backend/vault.py
+++ src/password_manager/backend/vault.py
@@ -13,10 +13,11 @@ class VaultKeyValue:
     # for example we may have 'username' with non-sensitive tr, 'password' with a sensitive str,
     # we may have a x509 private key which is sensitive, or a x509 public key which is not sensitive
     # attachments, etc.
-    def __init__(self, key: str, value: Any):
+    def __init__(self, key: str, value: str, hidden: bool):
         self._created = datetime.now(UTC)
         self.key: str = key
-        self.value: Any = value
+        self.value: str = value
+        self.hidden: bool = hidden
 
     def __eq__(self, other: object) -> bool:
         if not other:
